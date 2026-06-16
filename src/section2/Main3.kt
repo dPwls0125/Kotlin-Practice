c3e15f1ace9f11db1fc85790076dcb4ea26a6077
김예진 <yejin@gim-yejin-ui-MacBookPro.local>
2026-06-16 14:37:35 +0900

    [section2] 문자열 사용

--- src/section2/Main3.kt
+++ src/section2/Main3.kt
@@ -2,10 +2,18 @@ package section2
 
 fun main() {
     printAgeIfPerson(Person("김예진", 10))
+    val str : String = "ABC"
+    println(str[0])
 }
 
 
 fun printAgeIfPerson(obj: Any?) {
     val person = obj as? Person
-    println(person?.age)
+
+    println("이름: ${person?.name}")
+    println("나이: ${person?.age}")
+
+    println("""
+        안녕하쇼..! 
+    """.trimIndent())
 }
